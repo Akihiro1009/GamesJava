4f6c1d80380bc91b74eda27b5dc77b35f25aabd9
Akihiro1009 <akihiro_iwasaki@my.ofs.edu.sg>
2025-03-18 09:37:18 +0800

    done

--- Keiba/src/Race2.java
+++ Keiba/src/Race2.java
@@ -1,4 +1,4 @@
-import java.util.Random;
+import java.util.*;
 
 public class Race2 {
     public static String[] runner = {"h1", "h2", "h3", "h4", "h5", "h6", "h7", "h8", "h9", "h10", "h11", "h12"};
@@ -6,6 +6,29 @@ public class Race2 {
     public static String[] result = new String[runner.length];
 
     public static void setWinner(){
+        List<String> runnerList = Arrays.asList(runner);
+        Collections.shuffle(runnerList); //using shuffle() to shuffle the order.
+        result = runnerList.toArray(result);
+
+        //What is (String r : result)?
+        for (String r : result) {
+            System.out.println(r);
+        }
+
+        System.out.println("----------------------");
+
+        for (int j = 0; j < result.length; j++) {
+            System.out.println(result[j]);
+        }
+
+        System.out.println("----------------------");
+
+        //it is already converted into Array, so no need this execution.
+        /*for (int i = 1; i < result.length; i++) {
+            result[i] = runnerList.get(i - 1);
+            System.out.println(result[i]);
+        }*/
+
         //List<String>
 
 
